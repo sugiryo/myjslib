521f869259e5051fa3aaf3375af112eb670c2604
Ryo Sugihara <rsugihara@lyft.com>
2020-12-11 11:27:53 -0800

    Add more

--- myfunc.js
+++ myfunc.js
@@ -1,3 +1,25 @@
+function checkData(nonzeroQueryNameArray) {
+  let isDataOk = true;
+  let queryStateInfoStr = 'Query state:<ul>';
+  datasets.sort(function(a, b){return a['queryName'].localeCompare(b['queryName'])}).forEach(function(d) {
+    let desc = 'Succeeded';
+    let spanclass = 'query_succeeded';
+    if(d['state'] !== 'succeeded') {
+      isDataOk = false;
+      desc = 'Failed';
+      spanclass = 'query_failed';
+    } else if(nonzeroQueryNameArray.includes(d['queryName']) && d['count'] == 0) {
+      isDataOk = false;
+      desc = 'Succeeded but returned no row'
+      spanclass = 'query_empty';
+    }
+    queryStateInfoStr += '<li>' + d['queryName'] + ': ' + getElem('span', desc, [spanclass]).outerHTML + '</li>';
+  });
+  queryStateInfoStr += '</ul>';
+  document.getElementById("loadingdiv").innerHTML = getElem('div', queryStateInfoStr, ['queryState']).outerHTML;
+  return isDataOk;
+}
+
 function readQuery(queryName) {
   let dataSource = null;
   dataSource = datasets.filter(function(d) {
